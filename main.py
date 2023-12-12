c1f86f0fa0d4e2db174b3792d170a8f383d91521
ali <alilaser8080@gmail.com>
2023-12-12 23:14:58 +0330

    add pygame.display and windll

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import pgzrun
+import pygame.display
+from ctypes import windll
 
 WIDTH = 800
 HEIGHT = 600
+hwnd = pygame.display.get_wm_info()["window"]
+windll.user32.MoveWindow(hwnd, 550, 230, WIDTH, HEIGHT, False)
 
 pgzrun.go()
